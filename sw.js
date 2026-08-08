060dfdc4b4917a65ca26b0bdcdf8727b3ea74a2a
bladimirea28-spec <bladimirea28@gmail.com>
2026-08-08 00:39:36 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "mantenimiento-chanchan-v1";
+const ARCHIVOS_BASE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS_BASE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(
+        nombres.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // Estrategia: intenta la red primero (para no quedarte con versión vieja
+  // si hay internet), y si falla (sin señal), usa lo que haya en caché.
+  event.respondWith(
+    fetch(event.request)
+      .then((respuesta) => {
+        const copia = respuesta.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copia));
+        return respuesta;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
